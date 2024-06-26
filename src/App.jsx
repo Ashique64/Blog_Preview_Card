5a072663d3d4d6f171cb07aaad1930d1e8ce0924
Ashique <ashique9496@gmail.com>
2024-06-26 13:28:27 +0530

    made it perfect

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,13 @@ function App() {
     return (
         <>
             <div className="card">
-                <img src="\public\illustration-article.svg" alt="Loading" />
+                <img src="/illustration-article.svg" alt="Loading" />
                 <h6>Learning</h6>
                 <p className="date">Published 21 Dec 2023</p>
                 <h4>HTML & CSS foundations</h4>
                 <p className="para">These languages are the backbone of every website, defining structure, content, and presentation.</p>
                 <div className="person">
-                    <img src="\public\image-avatar.webp" alt="Loading" />
+                    <img src="/image-avatar.webp" alt="Loading" />
                     <h6>Greg Hooper</h6>
                 </div>
             </div>
